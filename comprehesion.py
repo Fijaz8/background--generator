d58d68622b723f2d73ccfc5e965435a316bd2f86
Fijaz <8086fijaz@gmail.com>
2024-01-29 16:24:18 +0530

    changing text

--- comprehesion.py
+++ comprehesion.py
@@ -4,8 +4,8 @@ Created on Mon Jan 15 12:46:37 2024
 
 @author: 8086f
 """
-
+li=['hello', "hi","sg"]
 some_list=["a","b","c","d","b","m","n","n"]
-
+print(li)
 some=[i for i in some_list if some_list.count(i)>1 ]
 print(some)
